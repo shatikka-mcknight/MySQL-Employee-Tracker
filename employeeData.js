79e4b8d8010c60813dbe389366e63ffc298bc856
Shatikka McKnight <32470118+shatikka-mcknight@users.noreply.github.com>
2021-06-11 18:41:39 -0400

    added mySql database along with employeesData.js file

---
+++ employeeData.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: 'DorKEr1910_',
+  database: 'employee_tracker_db',
+});
+
+const afterConnection = () => {
+  connection.query('SELECT * FROM employee', (err, res) => {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+};
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+  afterConnection();
+});
